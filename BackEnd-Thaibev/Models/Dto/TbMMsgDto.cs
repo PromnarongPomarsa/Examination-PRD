b42086f3006ce16c5326d4f1116326abf92c330a
LAPTOP-40EAPMHR\kanya <patipanc@metis.co.th>
2026-02-28 15:54:05 +0700

    add new dto for msg entity

---
+++ BackEnd-Thaibev/Models/Dto/TbMMsgDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEnd_Thaibev.Models.Dto
+{
+    public class TbMMsgDto
+    {
+        public int? id { get; set; }
+        public string? msgCode { get; set; }
+        public string? msgDesc { get; set; }
+        public string? createBy { get; set; }
+        public DateTime? createDate { get; set; }
+
+    }
+}
