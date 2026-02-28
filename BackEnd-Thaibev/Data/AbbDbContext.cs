d8e60fa3ebba64f1985e19f29b9851677f81a12e
LAPTOP-40EAPMHR\kanya <patipanc@metis.co.th>
2026-02-28 13:05:33 +0700

    add my dbcontext

---
+++ BackEnd-Thaibev/Data/AbbDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_Thaibev.Data
+{
+    public class AbbDbContext : DbContext
+    {
+        public AbbDbContext(DbContextOptions<AbbDbContext> options) : base(options)
+        {
+        }
+
+        // Define DbSet properties for your entities here
+        // Example:
+        // public DbSet<Product> Products { get; set; }
+
+    }
+}
