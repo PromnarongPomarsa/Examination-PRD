2aeeeba5e42806f80e766db44811ebaef1fcdab5
LAPTOP-40EAPMHR\kanya <patipanc@metis.co.th>
2026-03-02 01:14:19 +0700

    no message

--- FrontEnd-Thaibev/src/environments/environment.ts
+++ FrontEnd-Thaibev/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
    production: false,
    environmentName: 'DEV',
    buildNumber: 'V. 1.0.0',
-   apiUrl: '/api', // Uses proxy to http://localhost:7001   
+   apiUrl: 'https://rare-endurance-production-45b3.up.railway.app', // Uses proxy to http://localhost:7001   
    logLevel: 'debug'
 };
