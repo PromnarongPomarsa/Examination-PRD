2841949ddc2d0502c76f8448d4437e21e6995c67
LAPTOP-40EAPMHR\kanya <patipanc@metis.co.th>
2026-03-02 01:04:31 +0700

    no message

--- BackEnd-Thaibev/Program.cs
+++ BackEnd-Thaibev/Program.cs
@@ -51,7 +51,7 @@ app.UseCors(MyAllowSpecificOrigins);
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //db.Database.Migrate();
+    db.Database.Migrate();
 }
 
 
